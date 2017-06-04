68e9f88574a0c0e82ae1da333f2da093601480c6
凌中漢 <joelin@joemacbookair.local>
2017-06-04 22:15:53 +0800

    Initial Commit

---
+++ projecCppB10/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  projecCppB10
+//
+//  Created by 凌中漢 on 2017/6/4.
+//  Copyright © 2017年 joelin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
